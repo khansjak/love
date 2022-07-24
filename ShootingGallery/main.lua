7f01c0a98e9a7f687237d3418a2f491ed1a039a2
Javed Khan <jakhan@tekion.com>
2022-07-24 21:49:43 +0530

    Shooting Gallery- Added Start Menu and final touches

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -6,7 +6,7 @@ function love.load()
     target.x = 300 
     target.y = 300
     target.radius = 50
-    gameState =1
+    gameState =1 --This variables defines game session 1 -- main menu , 2 - in play
 
     score = 0 
     timer = 0
@@ -21,10 +21,16 @@ end
 
 --Refreshing screen on frames per second defined as dt
 function love.update(dt)
-    if timer >0 and gameState ==2 then
+    if timer >0 then
         timer = timer - dt
-    elseif timer<0 then
-        timer = 0 
+
+    end
+
+    if timer<0 then
+        timer = 0
+        gameState=1
+        
+        
     end
    
 end
@@ -36,36 +42,35 @@ function  love.draw()
     love.graphics.draw(sprites.sky, 0,0)
     love.graphics.setColor(1,1,1)
     love.graphics.setFont(gameFont)
-    love.graphics.print(score,0,0)
-    love.graphics.print(math.ceil(timer), 300, 0)
-    if gameState ==2 then
+    love.graphics.print("Score :"..score,0,0)
+    love.graphics.print("Timer : "..math.ceil(timer), 300, 0)
+    if gameState==2 then
         love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
-        love.graphics.draw(sprites.crosshairs,love.mouse.getX()-20,love.mouse.getY()-20)
-        love.graphics.printf("Click anywhere to begin",0,250,love.graphics.getWidth(),"center")
-    end
-    
+        love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)       
+    end    
 
-    
+    if gameState==1 then
+        love.graphics.printf("Click anywhere to begin !",0,250,love.graphics.getWidth(),"center")
+    end
 end
 
 
 function love.mousepressed(x, y, button, istouch, presses)
-    if button == 1 and timer > 0 and gameState == 2 then
-
+    if button == 1  and gameState == 2 then
         local mouseToTarget = distanceBetween(x,y,target.x,target.y)
         
-        love.audio.play(sound)
+        
         if mouseToTarget < target.radius then
             score = score+1
+            love.audio.play(sound)
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
         end
+    elseif button == 1 and gameState==1 then
+        gameState=2
+        timer = 10
+        score=0
     end
-
-    if button ==1 and gameState ==1 then
-        gameState =2 
-    end
-
 end
 
 function distanceBetween(x1,y1,x2,y2)
